0b4c75d9ea4a006973c3ccad96bd71a04ea35b65
CombatMC-YT <angelrouse508@icloud.com>
2026-09-13 12:24:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let cookieButton = document.getElementById("cookieButton")
+let cookieDisplay = document.getElementById("cookieDisplay")
+
+let upgraderClicker = document.getElementById("upgraderClicker")
+let autoClicker = document.getElementById("autoClicker")
